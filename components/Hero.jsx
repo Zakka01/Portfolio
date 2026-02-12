a35b075d756a98227f2585dc3e011b0065c353e9
Your Name <ahrabare8@gmail.com>
2026-02-12 04:38:08 +0100

    modify the Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,27 +5,30 @@ import Threads from './Threads'
 export default function Hero() {
 
     return (
-        <section className="flex flex-col gap-28">
-            <div className='w-full h-full px-8 md:px-8 relative top-26'>
+        <section className="flex flex-col">
+            <div className='w-full h-full px-8 md:px-4 relative top-28'>
                 <div className='flex flex-row justify-end w-full h-full'>
-                    <p className='text-[0.7rem] w-80 text-right uppercase text-neutral-400'>Designing branding materials and advertising visuals, with a sharp focus on social media and YouTube thumbnails that actually convert.</p>
+                    <p className='text-[0.65rem] w-75 text-right font-mono uppercase text-neutral-400'>Designing branding materials and advertising visuals, with a sharp focus on social media and YouTube thumbnails that actually convert</p>
                 </div>
-                <div className="flex flex-row items-end max-w-[1920px] mx-auto">
-                    <h1 className="cursor-default text-[9rem] text-neutral-100 mix-blend-difference w-[75%] tracking-tight leading-28">
-                        Designing clarity <span className="text-neutral-500"> in noisy spaces</span>
+                <div className="flex flex-row items-end justify-start max-w-[1920px] mx-auto pt-14">
+                    <h1 className="cursor-default font-medium text-[9rem] text-neutral-100 mix-blend-difference tracking-tight leading-30">
+                        Designing <br /> clarity <span className="text-neutral-500 font-mono"> in <br/> noisy spaces</span>
                     </h1>
-                    <div className='absolute left-[40%] -bottom-4 h-30 w-60 bg-neutral-100 rounded-xs'></div>
+                    <button className='absolute cursor-pointer font-mono uppercase flex justify-center items-center left-[54%] bottom-34 h-16 w-70 border border-neutral-400 text-neutral-400 hover:tracking-wider duration-300 hover:text-neutral-100'>
+                        let's connect w/ me
+                    </button>
                 </div>
 
+
                 <GradualBlur
-                target="page"
-                position="bottom"
-                height="7rem"
-                strength={1}
-                divCount={5}
-                curve="bezier"
-                exponential
-                opacity={1}
+                    target="page"
+                    position="bottom"
+                    height="7rem"
+                    strength={1}
+                    divCount={5}
+                    curve="bezier"
+                    exponential
+                    opacity={1}
                 />
             </div>
         </section>
